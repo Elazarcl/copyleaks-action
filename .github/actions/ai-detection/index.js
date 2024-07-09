5ad0efb628d4d4af84ba95a571a47aee52b19e11
Elazarcl <elazarb@copyleaks.com>
2024-07-09 17:59:37 +0300

    new workflow, plagiarism scan

--- .github/actions/ai-detection/index.js
+++ .github/actions/ai-detection/index.js
@@ -2,6 +2,32 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 const axios = require('axios');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+const sampleCode = `
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b'Hello, world!')
+
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
+    server_address = ('127.0.0.1', 8000)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting httpd server on {server_address[0]}:{server_address[1]}')
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run()
+`;
+
+function getBase64EncodedFileContent(content) {
+  return Buffer.from(content).toString('base64');
+}
 
 async function getTokenAsync(email, apiKey) {
   const loginUrl = 'https://id.copyleaks.com/v3/account/login/api';
@@ -23,11 +49,53 @@ async function getTokenAsync(email, apiKey) {
   }
 };
 
+async function scanForPlagiarismAsync(token, base64Content) {
+  const owner = github.context.repo.owner;
+  const repo = github.context.repo.repo;
+  const scanId = uuidv4();
+  const apiUrl = `https://api.copyleaks.com/v3/scans/submit/file/${scanId}`;
+  const webhookUrl = `https://api.github.com/repos/${owner}/${repo}/dispatches`;
+
+  const data = {
+    base64: base64Content,
+    filename: 'sample_code.py',
+    properties: {
+      webhooks: {
+        status: webhookUrl,
+        newResult: webhookUrl
+      }
+    }
+  };
+
+  try {
+    const response = await axios.put(apiUrl, data, {
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      },
+      validateStatus: function (status) {
+        return true;
+      }
+    });
+
+    if (response.status === 201) {
+      core.notice('Plagiarism scan request was successful.');
+      core.notice(`Webhook sent to ${webhookUrl}`);
+    } else {
+      core.warning(`Plagiarism scan request was not successful. Status code: ${response.status}`);
+    }
+  } catch (error) {
+    console.error('Error:', error.response ? error.response.data : error.message);
+  }
+};
+
 async function scanForAiDetectionAsync(token) {
   const scanId = 'scan_id';
-  const apiUrl = `https://api.copyleaks.com/v2/writer-detector/${scanId}/check`;
+  const apiUrl = `https://api.copyleaks.com/v2/writer-detector/source-code/${scanId}/check`;
   const data = {
-    text: 'Lions, often referred to as the "King of the Jungle," are among the most iconic and revered creatures in the animal kingdom. Known for their impressive strength, majestic appearance, and complex social structures, these magnificent big cats have fascinated humans for centuries. This essay delves into the biology, behavior, and cultural significance of lions, exploring why they continue to captivate our imaginations..'
+    text: sampleCode,
+    filename: 'testfile.py'
   };
 
   try {
@@ -44,18 +112,46 @@ async function scanForAiDetectionAsync(token) {
   }
 };
 
+async function getChangedFiles() {
+  const client = github.getOctokit(process.env.GITHUB_TOKEN);
+  const { owner, repo, number } = github.context.issue;
+
+  try {
+    const { data: files } = await client.pulls.listFiles({
+      owner: owner,
+      repo: repo,
+      pull_number: number
+    });
+
+    return files.map(file => ({
+      filename: file.filename,
+      contents_url: file.contents_url
+    }));
+
+  } catch (error) {
+    console.error('Error fetching changed files:', error.message);
+    throw error;
+  }
+}
+
 async function run() {
   try {
-    const email = core.getInput('email');
-    const apiKey = core.getInput('api_key');
+    const local = true;
+    const email = local ? 'elazarb@copyleaks.com' : core.getInput('email');
+    const apiKey = local ? '6f950dfa-9f97-48b0-9fae-8dc9dd2e484b' : core.getInput('api_key');
+
     core.notice('getting token ...');
     const token = await getTokenAsync(email, apiKey);
+
     core.notice('running scan ...');
-    const resData = await scanForAiDetectionAsync(token);
-    core.notice(`res: ${resData}`);
-    if (resData.summary.ai === 1) {
-      core.setFailed('AI detected in the code. Pipeline failed.');
-    }
+    const base64FileContent = getBase64EncodedFileContent(sampleCode);
+    await scanForPlagiarismAsync(token, base64FileContent);
+
+    // const resData = await scanForPlagiarismAsync(token, base64FileContent);
+    // core.notice(`res: ${resData}`);
+    // if (resData.summary.ai === 1) {
+    //   core.setFailed('AI detected in the code. Pipeline failed.');
+    // }
   } catch (error) {
     console.error('Failed to complete the request:', error.message);
   }
