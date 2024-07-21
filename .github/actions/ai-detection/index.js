d6babe8ecad0dc1aa16222a473246ebd09245d19
Elazarcl <elazarb@copyleaks.com>
2024-07-21 15:25:58 +0300

    changes

--- .github/actions/ai-detection/index.js
+++ .github/actions/ai-detection/index.js
@@ -1,172 +1,127 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const exec = require('@actions/exec');
-const axios = require('axios');
-const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
-const local = false;
 
-const sampleCode = `
-from http.server import BaseHTTPRequestHandler, HTTPServer
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
 
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write(b'Hello, world!')
+import { Octokit } from '@octokit/rest';
+import { createAppAuth } from '@octokit/auth-app';
 
-def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
-    server_address = ('127.0.0.1', 8000)
-    httpd = server_class(server_address, handler_class)
-    print(f'Starting httpd server on {server_address[0]}:{server_address[1]}')
-    httpd.serve_forever()
+const local = true;
 
-if __name__ == '__main__':
-    run()
-`;
+const privateKey = `-----BEGIN RSA PRIVATE KEY-----
+MIIEpQIBAAKCAQEA6bNiUap+nRxKs6ocwEO+JSlfRcuc+AtovxPq0Du4/nvDwX1A
+414sEPuGwK7ae1uqfgb+s8u+3INx2G2zaBXiX03e2XvOHy8xooW9yrsunsfbvZom
+GhUFFrEKtbEseiqdaHJrpZlSBeKtvH0QRTgriTPSLbJpt88xwZdbdcopaBfAFsru
+YgB1CwWH0ipdGvzDyw3ywZOmId9YIAZT2pBsuDFu8Lkn1FcYjQoea2WI14L21J7T
+wczNHLuPJRHKZ/U7sMN9O6zebeDWVa8wm7vXwTD/+qNXtstRCVL2MpsuKu3NJna6
+azApL8Yt0t6vdM5Np5JM35wnWdcNxIjVaOXTxwIDAQABAoIBACddRt9Yp5ERNegD
+bGpaPsoFqP2ZqWwCh/c/hJrkpGmBMV0DGw3xS9zkcTM9gKGstBAcX4Sreg+suF1r
+RP/wR7wj4rihaPvJuEOW4XwngVfPdE0AjRBxn/TNh29JU6OUUAVK2KUBKVbCct8/
+9aCjMHog13NJDROf+9KPCyHRFp5opz+299hpTBDAv2EBn2MXnvfeAJldOKAFT775
+meIowkPwfzuHk3MZR8XwTtsGHR9NxFvENnUu4i/GF0nKC6GWXJoOktaZ/m4tW2TN
+swNXy4xcdDHLGtdAXn6FVB2gGmQ7hfuo4YIl3eWFJ/6DqQvXDGaw1e+jEMxnHHCz
+hbvh0qECgYEA/89FMwNkFfAnsZs3dTlezobgBSaqFlDk3YmndYExahj4GYh+/TRy
+rki+dlglw+baU7QtZKgiWXRimcSAQEHaGwIvBbR+N4qTo/S5+o6gN1cHCIiWW+Rw
+FFdVtDhDDCuD2Jz3ARi8q1qRfYgVZ3wMhA/S7hwQOYQyH1zr15VVDhECgYEA6d/m
+9Otrlz99GyptCoLoJKIaBRavKxgmzSORzFXsjiSfCw7JLs5xHWiKWGKJxE3jG7pq
+MX9BadUxBeRCeTSB51J2k6dxseZAbcQSaYh8YUNpnJRRwIPr1fbA1On9aUEdVCum
+bMrwuB/ylXH2iTfbLOx666KdtMB5MpbUee7BTFcCgYEAsAPIU4+TwFyck2J/yFMP
+XwADCMhP5EcJEz5yUTHx9FT18WuwJT1/Y1h+ZSPF6IPh2hJx4X6nE/I3HiLNNGmb
+T1YzULOe3UymxfvQNA0LRMjG+CIzg0stq3FkcTr9N1SBxfxLISAKQcVxsnR+ddQv
+MHdK5be9mjXR3y6qwI3qFmECgYEAtFMJ/b/jFhxCtwZwStGCcnR2QYQrDbwydqs9
+SGLb4QFFi74byrYsKzOmr91SQgSX3pJOPNnUsuhJQzSrOE8O9i/z84cAt91Dwh6S
+38H3vPrAXJVTr29IEJKQPdoopNYq6fI6ANo83orrfOUA5O4nVS0E3dhlZq+dAy+e
+pmE7s0MCgYEAo/XnjdV7iGAzzKGiO8L0K9HvYjC9f5uWL6uq8KcJ4UsV2rHnE+Sp
+VXzRN4EgEkT1J8MLCv1tdQwLjURFq13z3vuTNOf2WbCND3YsGotFbIatJgT67AGs
+dYFCzV1ffsBs3ro3QTSuplu5z8HuZE1UK7gWYK6jP5nPZJS5NUHhZKY=
+-----END RSA PRIVATE KEY-----
+`
 
-function getBase64EncodedFileContent(content) {
-  return Buffer.from(content).toString('base64');
-}
+async function runCheckAsync() {
+  const appId = local ? '944685' : process.env.GITHUB_APP_ID;
+  const installationId = local ? '52790603' : process.env.GITHUB_INSTALLATION_ID;
+  const detailsUrl = "https://copyleaks.com";
 
-async function getTokenAsync(email, apiKey) {
-  const loginUrl = 'https://id.copyleaks.com/v3/account/login/api';
-  const credentials = {
-    email: email,
-    key: apiKey
-  };
+  const auth = createAppAuth({
+    appId,
+    privateKey,
+    installationId
+  });
 
-  try {
-    const response = await axios.post(loginUrl, credentials, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    return response.data.access_token;
-  } catch (error) {
-    console.error('Error getting token:', error.response ? error.response.data : error.message);
-    throw error;
-  }
-};
+  const installationAccessToken = await auth({ type: "installation" });
 
-async function scanForPlagiarismAsync(token, base64Content, ghToken) {
+  const octokit = new Octokit({
+    auth: installationAccessToken.token
+  });
+
   const owner = local ? 'Elazarcl' : github.context.repo.owner;
   const repo = local ? 'copyleaks-action' : github.context.repo.repo;
-  const scanId = uuidv4();
-  const apiUrl = `https://api.copyleaks.com/v3/scans/submit/file/${scanId}`;
-  const webhookUrl = `https://api.github.com/repos/${owner}/${repo}/dispatches`;
+  const sha = local ? '3b25b74e380d7242d7979fa009017c3e5aa689a9' : github.context.sha;
 
-  const data = {
-    base64: base64Content,
-    filename: 'sample_code.py',
-    properties: {
-      webhooks: {
-        status: webhookUrl,
-        newResult: webhookUrl,
-        newResultHeader: [['Authorization', `token ${ghToken}`]],
-      }
-    }
-  };
+  let checkRun;
+  try {
+    checkRun = await octokit.checks.create({
+      owner,
+      repo,
+      name: "Copyleaks scan",
+      head_sha: sha,
+      status: "in_progress",
+      started_at: new Date().toISOString(),
+      details_url: detailsUrl
+    });
+
+    core.notice(`Check run created: ${checkRun.data.html_url}`);
+  } catch (error) {
+    core.setFailed(`Failed to create check run: ${error.message}`);
+    return;
+  }
+
+  const checkRunId = checkRun.data.id;
+
+  await new Promise(resolve => setTimeout(resolve, 5000));
 
   try {
-    const response = await axios.put(apiUrl, data, {
-      headers: {
-        'Accept': 'application/json',
-        'Authorization': `Bearer ${token}`,
-        'Content-Type': 'application/json'
+    await octokit.checks.update({
+      owner,
+      repo,
+      check_run_id: checkRunId,
+      status: "in_progress",
+      output: {
+        title: "Copyleaks Scan",
+        summary: "The check is currently in progress...",
+        text: "This is an update indicating that the check is still running."
       },
-      validateStatus: function (status) {
-        return true;
-      }
+      details_url: detailsUrl
     });
 
-    if (response.status === 201) {
-      core.notice('Plagiarism scan request was successful.');
-      core.notice(`Webhook will be sent to ${webhookUrl}`);
-    } else {
-      core.warning(`Plagiarism scan request was not successful. Status code: ${response.status}`);
-    }
+    core.notice("Check run updated to in_progress");
   } catch (error) {
-    console.error('Error:', error.response ? error.response.data : error.message);
+    core.setFailed(`Failed to update check run: ${error.message}`);
+    return;
   }
-};
 
-async function scanForAiDetectionAsync(token) {
-  const scanId = 'scan_id';
-  const apiUrl = `https://api.copyleaks.com/v2/writer-detector/source-code/${scanId}/check`;
-  const data = {
-    text: sampleCode,
-    filename: 'testfile.py'
-  };
+  await new Promise(resolve => setTimeout(resolve, 5000));
 
   try {
-    const response = await axios.post(apiUrl, data, {
-      headers: {
-        'Accept': 'application/json',
-        'Authorization': `Bearer ${token}`,
-        'Content-Type': 'application/json'
-      }
-    });
-    return response.data;
-  } catch (error) {
-    console.error('Error:', error.response ? error.response.data : error.message);
-  }
-};
-
-async function getChangedFiles() {
-  const client = github.getOctokit(process.env.GITHUB_TOKEN);
-  const { owner, repo, number } = github.context.issue;
-
-  try {
-    const { data: files } = await client.pulls.listFiles({
-      owner: owner,
-      repo: repo,
-      pull_number: number
+    await octokit.checks.update({
+      owner,
+      repo,
+      check_run_id: checkRunId,
+      status: "completed",
+      conclusion: "failure",
+      completed_at: new Date().toISOString(),
+      output: {
+        title: "Custom Check",
+        summary: "The check has completed successfully.",
+        text: "Detailed information about the check results."
+      },
+      details_url: detailsUrl
     });
 
-    return files.map(file => ({
-      filename: file.filename,
-      contents_url: file.contents_url
-    }));
-
+    core.notice("Check run marked as completed");
   } catch (error) {
-    console.error('Error fetching changed files:', error.message);
-    throw error;
-  }
-}
-
-
-async function triggerDispatchEventAsync(ghToken) {
-  const owner = local ? 'Elazarcl' : github.context.repo.owner;
-  const repo = local ? 'copyleaks-action' : github.context.repo.repo;
-  const scanId = uuidv4();
-  const webhookUrl = `https://api.github.com/repos/${owner}/${repo}/dispatches`;
-
-  try {
-    const dispatchData = {
-      event_type: "scan-result",
-      client_payload: {
-        file: 'sample_code.py',
-        scan_id: scanId
-      }
-    };
-
-    const dispatchResponse = await axios.post(webhookUrl, dispatchData, {
-      headers: {
-        'Accept': 'application/vnd.github.v3+json',
-        'Authorization': `token ${ghToken}`,
-        'Content-Type': 'application/json'
-      }
-    });
-
-    if (dispatchResponse.status === 204) {
-      core.notice('Repository dispatch event was triggered successfully.');
-    } else {
-      core.warning(`Repository dispatch event was not successful. Status code: ${dispatchResponse.status}`);
-    }
-  } catch (error) {
-    console.error('Error:', error.response ? error.response.data : error.message);
+    core.setFailed(`Failed to mark check run as completed: ${error.message}`);
   }
 }
 
@@ -175,20 +130,10 @@ async function run() {
     const email = local ? 'elazarb@copyleaks.com' : core.getInput('email');
     const apiKey = local ? '6f950dfa-9f97-48b0-9fae-8dc9dd2e484b' : core.getInput('api_key');
     const ghToken = core.getInput('gh_token');
-    
-    core.notice('getting token ...');
-    const copyleaksToken = await getTokenAsync(email, apiKey);
 
     core.notice('running scan ...');
-    const base64FileContent = getBase64EncodedFileContent(sampleCode);
-    await triggerDispatchEventAsync(ghToken);
-    // await scanForPlagiarismAsync(copyleaksToken, base64FileContent, ghToken);
+    await runCheckAsync();
 
-    // const resData = await scanForPlagiarismAsync(token, base64FileContent);
-    // core.notice(`res: ${resData}`);
-    // if (resData.summary.ai === 1) {
-    //   core.setFailed('AI detected in the code. Pipeline failed.');
-    // }
   } catch (error) {
     console.error('Failed to complete the request:', error.message);
   }
